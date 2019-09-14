0962ffde867da0649f9030b62d5bd0de04dc64ea
meidl <meidanlong@qq.com>
2019-09-14 14:08:44 +0800

    Redis demo

---
+++ redis/src/main/java/com/meidl/redis/utils/RedisPoolUtils.java
@@ -0,0 +1,114 @@
+package com.meidl.redis.utils;
+
+import com.meidl.redis.pool.RedisPool;
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+
+@Slf4j
+public class RedisPoolUtils {
+
+    /**
+     * 设置key的有效期，单位是秒
+     * @param key
+     * @param exTime
+     * @return
+     */
+    public static Long expire(String key,int exTime){
+        Jedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.expire(key,exTime);
+        } catch (Exception e) {
+            log.error("expire key:{} error",key,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    //exTime的单位是秒
+    public static String setEx(String key,String value,int exTime){
+        Jedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.setex(key,exTime,value);
+        } catch (Exception e) {
+            log.error("setex key:{} value:{} error",key,value,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static String set(String key,String value){
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.set(key,value);
+        } catch (Exception e) {
+            log.error("set key:{} value:{} error",key,value,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static String get(String key){
+        Jedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.get(key);
+        } catch (Exception e) {
+            log.error("get key:{} error",key,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static Long del(String key){
+        Jedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.del(key);
+        } catch (Exception e) {
+            log.error("del key:{} error",key,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = RedisPool.getJedis();
+
+        RedisPoolUtils.set("keyTest","value");
+
+        String value = RedisPoolUtils.get("keyTest");
+
+        RedisPoolUtils.setEx("keyex","valueex",60*10);
+
+        RedisPoolUtils.expire("keyTest",60*20);
+
+        RedisPoolUtils.del("keyTest");
+
+
+//        String aaa = RedisPoolUtils.get(null);
+//        System.out.println(aaa);
+
+        System.out.println("end");
+
+
+    }
+}
