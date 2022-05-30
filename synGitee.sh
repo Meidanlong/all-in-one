f8cd5aa7ce1a2ee9ad682506263a9c8b6501480b
梅丹隆 <meidanlong@maoyan.com>
2022-05-30 14:47:32 +0800

    synchronize to Gitee

---
+++ synGitee.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# 推送代码
+git pull
+git add .
+git commit -m 'synchronize to Gitee master'
+
+# gitee
+git push -f "git@gitee.com:meidanlong/all-in-one.git" master
