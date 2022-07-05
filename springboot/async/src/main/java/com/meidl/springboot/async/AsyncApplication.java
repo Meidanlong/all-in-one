18c614d2a3fa04ca6f6e1118d85769a9a5c61b57
梅丹隆 <meidanlong@maoyan.com>
2022-07-05 11:28:52 +0800

    优化聚合项目

---
+++ springboot/async/src/main/java/com/meidl/springboot/async/AsyncApplication.java
@@ -0,0 +1,17 @@
+package com.meidl.springboot.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: meidanlong
+ * @date: 2022/7/5 10:37
+ */
+@SpringBootApplication
+public class AsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApplication.class);
+    }
+}
