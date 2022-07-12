cd363b196a8fefff51a765164a7c556d2d67ca43
梅丹隆 <meidanlong@maoyan.com>
2022-07-12 12:47:19 +0800

    集成dubbo成功

--- springcloud-alibaba/consumer/src/main/java/com/meidl/springcloudalibaba/consumer/ConsumerApplication.java
+++ springcloud-alibaba/consumer/src/main/java/com/meidl/springcloudalibaba/consumer/ConsumerApplication.java
@@ -1,8 +1,10 @@
 package com.meidl.springcloudalibaba.consumer;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description:
@@ -11,6 +13,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDubbo
+@ComponentScan(value = "com.meidl.springcloudalibaba.consumer.reference")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
