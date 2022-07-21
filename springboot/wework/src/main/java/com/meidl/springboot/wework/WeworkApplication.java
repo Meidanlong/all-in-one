4efcf6e5e90d0f8106ca447223089c1a70c010e7
梅丹隆 <meidanlong@maoyan.com>
2022-07-21 14:58:12 +0800

    初始化企业微信项目

---
+++ springboot/wework/src/main/java/com/meidl/springboot/wework/WeworkApplication.java
@@ -0,0 +1,16 @@
+package com.meidl.springboot.wework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: meidanlong
+ * @date: 2022/7/21 14:51
+ */
+@SpringBootApplication
+public class WeworkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeworkApplication.class, args);
+    }
+}
