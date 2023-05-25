4c86aded49c89ab9222a22aa7cf7e09208bce000
meidanlong <meidanlong@gmail.com>
2023-05-25 10:37:44 +0800

    添加springboot下alibaba模块

---
+++ springboot/alibaba/src/main/java/com/mdl/springboot/alibaba/AlibabaApplication.java
@@ -0,0 +1,12 @@
+package com.mdl.springboot.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaApplication.class);
+    }
+}
