2779365f2b8fd7f1106005ccd9df69b4e6f0d848
meidanlong <meidanlong@gmail.com>
2023-05-25 14:30:00 +0800

    完善代码结构

---
+++ springboot/aigc/src/main/java/com/mdl/springboot/aigc/service/IChatGPTService.java
@@ -0,0 +1,4 @@
+package com.mdl.springboot.aigc.service;
+
+public interface IChatGPTService {
+}
---
+++ springboot/aigc/src/main/java/com/mdl/springboot/aigc/service/impl/ChatGPTServiceImpl.java
@@ -0,0 +1,9 @@
+package com.mdl.springboot.aigc.service.impl;
+
+import com.mdl.springboot.aigc.service.IChatGPTService;
+
+public class ChatGPTServiceImpl implements IChatGPTService {
+
+
+
+}
